2506bcb6a6d0f365b82c877846f5c3340e399963
Jaime Rosique Anton <JROSANT@upv.edu.es>
2025-05-18 17:51:28 +0200

    Registro fixed

--- Proyecto-IPC/src/application/JavaFXMLApplication.java
+++ Proyecto-IPC/src/application/JavaFXMLApplication.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package application;
 
+import java.io.IOException;
 import java.util.HashMap;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -12,115 +8,62 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class JavaFXMLApplication extends Application {
-    
+
     private static Scene scene;
     private static HashMap<String, Parent> roots = new HashMap<>();
     private static HashMap<String, Object> controllers = new HashMap<>();
-    
+
     @Override
     public void start(Stage stage) throws Exception {
-        //======================================================================
-        // 1- creación del grafo de escena a partir del fichero FXML
-        FXMLLoader loader= new  FXMLLoader(getClass().getResource("InicioSesion.fxml"));
-        Parent root = loader.load();
-        //======================================================================
-        // 2- creación de la escena con el nodo raiz del grafo de escena
-        Scene scene = new Scene(root);
-        //======================================================================
-        // 3- asiganación de la escena al Stage que recibe el metodo 
-        //     - configuracion del stage
-        //     - se muestra el stage de manera no modal mediante el metodo show()
-        
-        //Crea la escena a partir del fichero FXML para 
-        //Iniciar Sesion
-        loader = new FXMLLoader(getClass().getResource("InicioSesion.fxml"));
-        root = loader.load();
-        roots.put("IniciarSesion", root);
-        controllers.put("IniciarSesion", loader.getController());
-        //Crea la escena a partir del fichero FXML para
-        //Registro
-        loader = new FXMLLoader(getClass().getResource("Registro.fxml"));
-        root = loader.load();
-        roots.put("Registro", root);
-        controllers.put("Registro", loader.getController());
-        //Crea la escena a partir del fichero FXML para 
-        //Menu
+        // Cargar todas las escenas
+        cargarEscena("InicioSesion.fxml", "IniciarSesion");
+        cargarEscena("Registro.fxml", "Registro");
+
         /*
-        loader = new FXMLLoader(getClass().getResource("Menu.fxml"));
-        root = loader.load();
-        roots.put("Menu", root);
-        controllers.put("Menu", loader.getController());
-        //Crea la escena a partir del fichero FXML para 
-        //ActualizarDatos
-        loader = new FXMLLoader(getClass().getResource("ActualizarDatos.fxml"));
-        root = loader.load();
-        roots.put("ActualizarDatos", root);
-        controllers.put("ActualizarDatos", loader.getController());
-        //Crea la escena a partir del fichero FXML para 
-        //Añadir gastos
-        loader = new FXMLLoader(getClass().getResource("AddGastos.fxml"));
-        root = loader.load();
-        roots.put("AddGastos", root);
-        controllers.put("AddGastos", loader.getController());
-        //Crea la escena a partir del fichero FXML para 
-        //Añadir categoria
-        loader = new FXMLLoader(getClass().getResource("AddCat.fxml"));
-        root = loader.load();
-        roots.put("AddCat", root);
-        controllers.put("AddCat", loader.getController());
-        //Crea la escena a partir del fichero FXML para 
-        //Ver cuenta
-        loader = new FXMLLoader(getClass().getResource("VerCuenta.fxml"));
-        root = loader.load();
-        roots.put("VerCuenta", root);
-        controllers.put("VerCuenta", loader.getController());
-        //Crea la escena a partir del fichero FXML para 
-        //Ver cuenta Año
-        loader = new FXMLLoader(getClass().getResource("VerCuentaAño.fxml"));
-        root = loader.load();
-        roots.put("VerCuentaAño", root);
-        controllers.put("VerCuentaAño", loader.getController());
-        //Crea la escena a partir del fichero FXML para 
-        //Ver cuenta Cat
-        loader = new FXMLLoader(getClass().getResource("VerCuentaCat.fxml"));
-        root = loader.load();
-        roots.put("VerCuentaCat", root);
-        controllers.put("VerCuentaCat", loader.getController());
-        //Crea la escena a partir del fichero FXML para 
-        //Lista de cargos
-        loader = new FXMLLoader(getClass().getResource("ListaCargos.fxml"));
-        root = loader.load();
-        roots.put("ListaCargos", root);
-        controllers.put("ListaCargos", loader.getController());
+        cargarEscena("Menu.fxml", "Menu");
+        cargarEscena("ActualizarDatos.fxml", "ActualizarDatos");
+        cargarEscena("AddGastos.fxml", "AddGastos");
+        cargarEscena("AddCat.fxml", "AddCat");
+        cargarEscena("VerCuenta.fxml", "VerCuenta");
+        cargarEscena("VerCuentaAño.fxml", "VerCuentaAño");
+        cargarEscena("VerCuentaCat.fxml", "VerCuentaCat");
+        cargarEscena("ListaCargos.fxml", "ListaCargos");
         */
-        //Creación de la escena con el nodo raíz del grafo de escena
+
+        // Crear la escena inicial
         scene = new Scene(roots.get("IniciarSesion"));
+
+        // Cargar CSS si lo tienes
         //String css = this.getClass().getResource("/styles/EstilosLayout.css").toExternalForm();
         //scene.getStylesheets().add(css);
-        //Asignación de la escena al Stage que recibe el metodo, incluyendo titulos y dimensiones
+
+        // Configuración de la ventana principal
         stage.setMinWidth(500);
         stage.setMinHeight(400);
-        
         stage.setScene(scene);
         stage.setTitle("Inicio de sesión");
         stage.show();
     }
-    
+
+    private void cargarEscena(String nombreFXML, String clave) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(nombreFXML));
+        Parent root = loader.load();
+        roots.put(clave, root);
+        controllers.put(clave, loader.getController());
+    }
+
     public static void setRoot(Parent root) {
         scene.setRoot(root);
-        Stage st = (Stage)scene.getWindow();
     }
-    
+
     public static void setRoot(String clave) {
         Parent root = roots.get(clave);
         if (root != null) {
             setRoot(root);
-            
+
             Stage stage = (Stage) scene.getWindow();
-            switch(clave) {
-                
+            switch (clave) {
                 case "IniciarSesion":
                     stage.setMinWidth(500);
                     stage.setMinHeight(400);
@@ -152,15 +95,7 @@ public class JavaFXMLApplication extends Application {
                     stage.setTitle("Añadir Categoria");
                     break;
                 case "VerCuenta":
-                    stage.setMinWidth(922);
-                    stage.setMinHeight(830);
-                    stage.setTitle("Ver Cuenta");
-                    break;
                 case "VerCuentaAño":
-                    stage.setMinWidth(922);
-                    stage.setMinHeight(830);
-                    stage.setTitle("Ver Cuenta");
-                    break;
                 case "VerCuentaCat":
                     stage.setMinWidth(922);
                     stage.setMinHeight(830);
@@ -172,31 +107,21 @@ public class JavaFXMLApplication extends Application {
                     stage.setTitle("Lista Cargos");
                     break;
             }
-        }
-        else {
-            System.err.printf("No se encuentra la escena: %s", clave);
+        } else {
+            System.err.printf("No se encuentra la escena: %s%n", clave);
         }
     }
-    
+
     public static Object getController(String clave) {
-        Object controller = controllers.get(clave);
-        if(controller != null){
-            return controller;
-        }
-        else {
-            System.err.printf("No se encuentra el controller: %s", clave);
+        if (controllers.containsKey(clave)) {
+            return controllers.get(clave);
+        } else {
+            System.err.printf("No se encuentra el controller: %s%n", clave);
             return null;
         }
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
-        
     }
-
-
-    
 }
